0d8e1deca2a8ef97d95fc1681ce4470c7eae7fdb
YaelVilla <yaelvillasenor07@gmail.com>
2024-09-12 22:54:25 -0600

    Update unidad2.java

---
+++ Tarea3/unidad2.java
@@ -0,0 +1,76 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // SECCION 1 TAREA 03 LIBRO FAVORITO
+
+        System.out.println("El titutlo de mi libro favorito es: El arte de la guerra");
+
+        System.out.println("El autor es: Tzun Tzu");
+
+        System.out.println("Se publico en el año: 1772");
+
+        System.out.println("Es un libro del genero: Drama");
+
+        System.out.println("Tiene un total de: 128");
+
+        //SECCION 2 TAREA 03  país que deseas visitar
+
+        String nombreDelPais = "Canada";
+
+        String capital = "Ottawa";
+
+        String idiomaOficial = "Inglés";
+
+        String moneda = "Dolar";
+
+        int poblacion = 38930000;
+
+        System.out.println("El nombre del pais que quisiera visitar es: " + nombreDelPais);
+
+        System.out.println("La capital del pais que quisiera visitar es: " + capital);
+
+        System.out.println("El idioma oficial del país que quisiera visitar es: " + idiomaOficial);
+
+        System.out.println("La moneda del páis que quisiera visitar es: " + moneda);
+
+        System.out.println("La poblacion del pais es: " + poblacion);
+
+        //SECCION 3 TAREA 03  Información de un personaje histórico favoritO
+
+        String nombreCompleto = ("Elvis Presley");
+
+        LocalDate fechaNacimiento = LocalDate.of(1935, 1, 8);
+
+        String nacionalidad = ("estadounidense");
+
+        String ocupacion = ("cantante");
+
+        String logroDestacado = ("galardones de oro, platino y multiplatino");
+
+        System.out.println("El nombre de mi personaje historico favorito es: " + nombreCompleto);
+
+        System.out.println("Su fecha de nacimiento fue: " + fechaNacimiento);
+
+        System.out.println("Su nacionalidad fue: " + nacionalidad);
+
+        System.out.println("Su ocupacion fue: " + ocupacion);
+
+        System.out.println("Sus logros destacados fueron: " + logroDestacado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
